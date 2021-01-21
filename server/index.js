79e054512fa692c156cab65c179a68112c45d54a
Thomas Domingo <tdomingo90@gmail.com>
2021-01-21 11:00:54 -0800

    create seeding file and script, merge Listing schema into index

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 /* eslint-disable no-console */
 const express = require('express');
+const db = require('../database/index.js');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+
+app.get('/api/more', (req, res) => {
+  db.Listing.find()
+    .then((listingArrays) => {
+      const i = (Math.floor(Math.random() * 100));
+      res.status(200).send(listingArrays[i].nearby);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
 
 app.listen(port, () => {
   console.log(`More Places to Stay listening on port ${port}`);
